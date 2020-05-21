4a4490762ba5a8b800161940fed7bfcbdd23588a
sarthakkundra <sarthakkundra21@gmail.com>
2020-05-22 00:45:22 +0530

    Added user UI

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React, { Fragment, Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import axios from 'axios';
+
+// Components
 import Navbar from "./components/layouts/Navbar";
 import Users from "./components/users/Users";
 import User from "./components/users/User";
 import Search from "./components/users/Search";
 import Alert from "./components/layouts/Alert";
 import About from "./components/pages/About";
+
+// Main CSS
 import "./App.css";
-const axios = require("axios");
 
 class App extends Component {
   state = {
     users: [],
+    repos: [],
     user: {},
     loading: false,
     alert: null,
@@ -34,9 +39,19 @@ class App extends Component {
       `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
     this.setState({ user: res.data, loading: false });
-    console.log(res.data);
-    console.log('in scope')
   };
+
+    // Get repos of single user
+    getUserRepos = async (username) => {
+      this.setState({ loading: true });
+  
+      const res = await axios.get(
+        `https://api.github.com/users/${username}/repos?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      );
+      this.setState({ repos: res.data, loading: false });
+    };
+
+  // Function to give alerts
   setAlert = (msg, type) => {
     this.setState({ alert: { msg, type } });
     setTimeout(() => {
@@ -47,7 +62,7 @@ class App extends Component {
   clearUsers = () => this.setState({ users: [], loading: false });
 
   render() {
-    const { users, user, loading } = this.state;
+    const { users, user, repos, loading } = this.state;
     return (
       <Router>
         <div className='App'>
@@ -73,13 +88,15 @@ class App extends Component {
               <Route exact path='/about' component={About} />
               <Route
                 exact
-                path='/users/:login'
+                path='/user/:login'
                 render={(props) => (
                   <User
-                    {...props}
+                    { ...props }
                     getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
                     loading={loading}
                     user={user}
+                    repos={repos}
                   />
                 )}
               />
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,9 +1,22 @@
-import React, { Component } from "react";
+import React, { Fragment, Component } from "react";
+import { Link } from "react-router-dom";
+import Spinner from "../layouts/Spinner.js";
+import Repos from '../repos/Repos'
+import PropTypes from "prop-types";
 
 export class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
+    this.props.getUserRepos(this.props.match.params.login);
   }
+
+  static propTypes = {
+    loading: PropTypes.bool,
+    user: PropTypes.object.isRequired,
+    getUser: PropTypes.func.isRequired,
+    getUserRepos: PropTypes.func.isRequired
+  };
+
   render() {
     const {
       name,
@@ -18,9 +31,59 @@ export class User extends Component {
       public_repos,
       public_gists,
       hireable,
+      company,
     } = this.props.user;
+
+    const { loading, repos } = this.props;
+    if (loading) return <Spinner />;
+
     return (
-      <div>{name}</div>
+      <Fragment>
+        <Link to='/' className='btn btn-light'>
+          Back to Search
+        </Link>
+        Hireable:{" "}
+        {hireable ? (
+          <i className='fas fa-check text-success' />
+        ) : (
+          <i className='fas fa-times-circle text-danger' />
+        )}
+        <div className='card grid-2'>
+          <div className='all-center'>
+            <img
+              src={avatar_url}
+              className='round-image'
+              style={{ width: "150px" }}
+            />
+            <h1>{name}</h1>
+            <p>Location: {location}</p>
+          </div>
+          <div>
+          {bio && (
+            <Fragment>
+              <p>Bio: {bio}</p>
+            </Fragment>
+          )}
+          <a href={html_url} className='btn btn-dark my-1'>
+            View Github Profile
+          </a>
+          <ul>
+            <li>{login && <Fragment>Username: {login}</Fragment>}</li>
+            <li>{company && <Fragment>Company: {company}</Fragment>}</li>
+            <li>{blog && <Fragment>Website: {blog}</Fragment>}</li>
+          </ul>
+        </div>
+        </div>
+        <div className='card text-center'>
+          <div className='badge badge-primary'>Followers: {followers}</div>
+          <div className='badge badge-success'>Following: {following}</div>
+          <div className='badge badge-light'>
+            Public Repositories: {public_repos}
+          </div>
+          <div className='badge badge-dark'>Public Gists: {public_gists}</div>
+            </div>
+        <Repos repos={repos} />
+      </Fragment>
     );
   }
 }
