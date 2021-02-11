7d5cd758d5cc8263c5b213fdeaec1f1a8cb747e3
singhayushh <coding.ayush@gmail.com>
2021-02-11 14:14:14 +0530

    User Card css animation on hover

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -4,12 +4,17 @@ import { Link } from 'react-router-dom';
 const UserItem = ({ user: { avatar_url, html_url, login } }) => {
   return (
     <div className='card text-center'>
-      <img
-        src={avatar_url}
-        alt=''
-        className='round-img'
-        style={{ width: "60px" }}
-      />
+      <div
+        className="img-wrapper"
+        style={imgStyle}
+      >
+        <img
+          src={avatar_url}
+          alt=''
+          className='round-img'
+          style={{ width: "120px", transition: "all 0.4s ease" }}
+        />
+      </div>
       <h3>{login}</h3>
 
       <div>
@@ -19,6 +24,19 @@ const UserItem = ({ user: { avatar_url, html_url, login } }) => {
       </div>
     </div>
   );
+};
+
+const imgStyle = {
+  height: "80px",
+  width: "80px",
+  borderRadius: "50%",
+  overflow: "hidden",
+  margin: "1rem auto",
+  padding: "1rem",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  border: "5px solid var(--light-color)"
 };
 
 export default UserItem;
